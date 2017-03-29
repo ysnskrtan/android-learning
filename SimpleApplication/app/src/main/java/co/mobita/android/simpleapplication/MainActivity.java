ec243930eb10347d899e56d3544a15625179ea08
Yasin Şükrü Tan <yasinsukrutan@gmail.com>
2017-03-29 15:38:09 +0300

    Click Funtionality Added
    Click Funtionality Added the order button

--- SimpleApplication/app/src/main/java/co/mobita/android/simpleapplication/MainActivity.java
+++ SimpleApplication/app/src/main/java/co/mobita/android/simpleapplication/MainActivity.java
@@ -2,6 +2,8 @@ package co.mobita.android.simpleapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    // This function runs When the button pressed
+    public void submitOrder(View view){
+        displayChange(1);
+    }
+
+    // This function changes the quantity
+    private void displayChange(int number){
+        TextView quantityTextView = (TextView)findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
     }
 }
