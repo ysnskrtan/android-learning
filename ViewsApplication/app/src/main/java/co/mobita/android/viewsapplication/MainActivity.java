433038215865aea9b2bb8cc270220bc427787597
Yasin Şükrü Tan <yasinsukrutan@gmail.com>
2017-03-29 09:54:10 +0300

    Views Application Created
    We try some view elements of android, like textview, imageview, button and checkbox

---
+++ ViewsApplication/app/src/main/java/co/mobita/android/viewsapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package co.mobita.android.viewsapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
