57e0086407979e7c60a8e552959cb746dfbeecf9
Yasin Şükrü Tan <yasinsukrutan@gmail.com>
2017-04-01 20:53:26 +0300

    Simple Application V2
    We use scroll view, check box view added and the functionality of program is developed.

--- NestedViewGroupsApplication/app/src/main/java/co/mobita/android/nestedviewgroupsapplication/MainActivity.java
+++ NestedViewGroupsApplication/app/src/main/java/co/mobita/android/nestedviewgroupsapplication/MainActivity.java
@@ -4,6 +4,8 @@ import java.text.NumberFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import co.mobita.android.nestedviewgroupsapplication.R;
@@ -21,6 +23,34 @@ public class MainActivity extends AppCompatActivity {
         TextView quantity = (TextView)findViewById(R.id.quantity_text_view);
         int amount = Integer.parseInt(quantity.getText().toString());
         return amount;
+    }
+
+    /*
+        This functions returns the given name
+     */
+    protected String getName(){
+        EditText name = (EditText) findViewById(R.id.name_edit_text);
+        return name.getText().toString();
+    }
+
+    /*
+        This function returns the coffee is with chocolate or not
+     */
+    protected String withChocolate(){
+        CheckBox chocolate = (CheckBox) findViewById(R.id.chocolate_check_box);
+        if(chocolate.isChecked())
+            return "true";
+        return "false";
+    }
+
+    /*
+        This function returns the coffee is with whipped cream or not
+     */
+    protected String withCream(){
+        CheckBox cream = (CheckBox) findViewById(R.id.cream_check_box);
+        if(cream.isChecked())
+            return "true";
+        return "false";
     }
 
     // increase the amount
@@ -45,14 +75,33 @@ public class MainActivity extends AppCompatActivity {
     // This function runs When the button pressed
     public void submitOrder(View view){
         int numberOfCoffees = getAmount();
-        displayChange(numberOfCoffees);
+        orderSummary(numberOfCoffees);
     }
 
-    // This function changes the quantity
-    private void displayChange(int number){
-        int price = number * 5;
+    /*
+        This function summarize the order
+        @param number: amount of ordered coffee
+     */
+    private void orderSummary(int number){
+        String messageForName = "Name: " + getName() + "\n";
+        String whippedCream = "Add whipped cream? " + withCream() + "\n";
+        String chocolate = "Add chocolate? " + withChocolate() + "\n";
+        String quantity = "Quantity: " + number + "\n";
+        String prize = "Total: " + NumberFormat.getCurrencyInstance().format((double) calculatePrize(number)) + "\n";
         String thanksMessage = "Thank you!";
         TextView priceTextView = (TextView)findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format((double) price) + "\n" + thanksMessage);
+        priceTextView.setText(messageForName + whippedCream + chocolate + quantity + prize + thanksMessage);
+    }
+
+    private int calculatePrize(int quantity){
+        int aCupPrize = 5;
+
+        if (Boolean.parseBoolean(withChocolate()))
+            aCupPrize += 2;
+
+        if (Boolean.parseBoolean(withCream()))
+            aCupPrize += 1;
+
+        return quantity * aCupPrize;
     }
 }
